411f93750bb1e8244ee8e5fdf14dc7ae3313d262
zeroZshadow <martijn_gerkes@hotmail.com>
2017-01-21 03:24:01 +0000

    Oops, that should be a reference, not a value

--- lobby.go
+++ lobby.go
@@ -9,7 +9,7 @@ type roomConstructor func(RoomID) Room
 // Lobby collection of rooms
 type Lobby struct {
 	server *Server
-	rooms  map[RoomID]roomInfo
+	rooms  map[RoomID]*roomInfo
 	sync.RWMutex
 }
 
@@ -24,7 +24,7 @@ var RoomLobby *Lobby
 func initializeLobby(server *Server) {
 	RoomLobby = &Lobby{
 		server: server,
-		rooms:  make(map[RoomID]roomInfo),
+		rooms:  make(map[RoomID]*roomInfo),
 	}
 }
 
@@ -39,33 +39,31 @@ func (lobby *Lobby) NewRoom(id RoomID, constructor roomConstructor) *RoomFront {
 	if _, ok := lobby.rooms[id]; !ok {
 		// Create the new room
 		front = newRoomFront(constructor(id))
-		lobby.rooms[id] = roomInfo{
+		lobby.rooms[id] = &roomInfo{
 			room: front,
 		}
-	} else {
-		lobby.server.log.Println("Trying to create already existing room ", id)
-	}
 
-	// Start up the goroutine that will handle the room
-	front.start()
+		// Start up the goroutine that will handle the room
+		front.start()
+	}
 
 	return front
 }
 
 // JoinRoom Add user to room
-func (lobby *Lobby) JoinRoom(id RoomID, user User) error {
+func (lobby *Lobby) JoinRoom(id RoomID, user User) (*RoomFront, error) {
 	lobby.Lock()
 	defer lobby.Unlock()
 
 	roomInfo, ok := lobby.rooms[id]
 	if !ok {
-		return errors.New(fmt.Sprint("Lobby is unable to join room", id))
+		return nil, errors.New(fmt.Sprint("Lobby is unable to join room", id))
 	}
 
 	roomInfo.userCount++
 	roomInfo.room.addUser(user)
 
-	return nil
+	return roomInfo.room, nil
 }
 
 // LeaveRoom Remove user from room and cleanup if required
